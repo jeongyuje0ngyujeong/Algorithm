551c694e60312a56326c8a8d9233fd6dbfaa0362
jeongyuje0ngyujeong <164482637+jeongyuje0ngyujeong@users.noreply.github.com>
2024-04-11 19:53:21 +0900

    [Silver I] Title: 부등호, Time: 156 ms, Memory: 15872 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -2,19 +2,16 @@ import java.io.*;
 
 public class Main {
     private static int signSize;
-    private static int[] arr;
     private static  String[] signArr;
     private static boolean[] isVisited;
     private static long max = -1;
     private static long min = 9999999999L;
-
-
-    private static StringBuilder sb = new StringBuilder();
+    private static  long answer = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         signSize = Integer.parseInt(br.readLine());
-        arr = new int[signSize + 1];
+
         signArr = br.readLine().split(" ");
         isVisited = new boolean[10];
 
@@ -28,35 +25,29 @@ public class Main {
 
     private static void dfs(int depth) {
         if (depth > signSize) {
-            for (int i : arr) {
-                sb.append(i);
-            }
-
-            long checkNum = Long.parseLong(sb.toString());
-            if(checkNum > max) max = checkNum;
-            if(checkNum < min) min = checkNum;
-            sb = new StringBuilder();
+            if(answer > max) max = answer;
+            if(answer < min) min = answer;
             return;
         }
 
         for (int i = 0; i < 10; i++) {
             if (!isVisited[i]) {
-                arr[depth] = i;
+                long prev = answer % 10;
+                answer = answer * 10 + i;
                 isVisited[i] = true;
                 if (depth == 0) dfs(depth + 1);
-                else if (depth <= signSize && check(arr[depth - 1], arr[depth], signArr[depth - 1])) dfs(depth + 1);
+                else if (check(prev, answer % 10, signArr[depth - 1])) dfs(depth + 1);
                 isVisited[i] = false;
+                answer /= 10;
             }
         }
     }
 
-    private static boolean check(int a, int b, String c) {
+    private static boolean check(long a, long b, String c) {
         if (c.equals(">")) {
-            if (a < b) return false;
+            return a > b;
         } else {
-            if (a > b) return false;
+            return a < b;
         }
-
-        return true;
     }
 }
