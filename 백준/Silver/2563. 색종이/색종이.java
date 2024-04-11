7a990fe7ebe8dcb610795b0dd7cec7f370776f5d
jeongyuje0ngyujeong <164482637+jeongyuje0ngyujeong@users.noreply.github.com>
2024-04-12 01:52:50 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -8,24 +8,22 @@ public class Main {
         int paperNum = Integer.parseInt(br.readLine());
         int[][] drawingPaper = new int[100][100];
         
+        int cnt = 0;
         for (int i = 0; i < paperNum; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int w = Integer.parseInt(st.nextToken());
             int h = Integer.parseInt(st.nextToken());
+            
             for (int j = h; j < h + 10; j++) {
                 for (int k = w; k < w + 10; k++) {
-                    if (drawingPaper[j][k] == 0) drawingPaper[j][k] = 1;
+                    if (drawingPaper[j][k] == 0) {
+                        drawingPaper[j][k] = 1;
+                        cnt++;
+                    }
                 }
             }
+            
         }
-
-        int cnt = 0;
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                if (drawingPaper[i][j] == 1) cnt++;
-            }
-        }
-
         System.out.println(cnt);
     }
 }
