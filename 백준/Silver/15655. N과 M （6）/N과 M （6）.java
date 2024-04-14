81d05a1bb9baa3d202b17282443d096cd83fef4f
jeongyuje0ngyujeong <164482637+jeongyuje0ngyujeong@users.noreply.github.com>
2024-04-14 17:18:25 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int[] nums;
+    private static int[] check;
+    private static boolean[] isVisited;
+    private static int m;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int n = Integer.parseInt(str.split(" ")[0]);
+        m = Integer.parseInt(str.split(" ")[1]);
+        nums = new int[n];
+        check = new int[m];
+        isVisited = new boolean[n];
+
+        str = br.readLine();
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(str.split(" ")[i]);
+        }
+
+        Arrays.sort(nums);
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+    
+    private static void dfs(int depth, int curIdx) {
+        if (depth == m) {
+            for (int i : check) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = curIdx; i < nums.length; i++) {
+            if (!isVisited[i]) {
+                isVisited[i] = true;
+                check[depth] = nums[i];
+                dfs(depth + 1, i + 1);
+                isVisited[i] = false;
+            }
+        }
+    }
+}
