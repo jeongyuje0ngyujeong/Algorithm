e37f3ff9d3f46ff4fe664b2fc524b9536e7b5f9e
jeongyuje0ngyujeong <164482637+jeongyuje0ngyujeong@users.noreply.github.com>
2024-04-20 23:39:50 +0900

    [Silver II] Title: 타일링, Time: 168 ms, Memory: 15032 KB -BaekjoonHub

---
+++ 백준/Silver/1793. 타일링/타일링.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger[] memo = new BigInteger[251];
+        memo[0] = BigInteger.valueOf(1);
+        memo[1] = BigInteger.valueOf(1);
+        memo[2] = BigInteger.valueOf(3);
+        BigInteger two = new BigInteger("2");
+        
+        for (int i = 3; i < 251; i++) {
+            memo[i] = (memo[i - 1]).add(two.multiply(memo[i - 2]));
+        }
+
+        String n;
+        while ((n = br.readLine())!= null) {
+            int width = Integer.parseInt(n);
+            System.out.println(memo[width]);
+        }
+    }
+}
