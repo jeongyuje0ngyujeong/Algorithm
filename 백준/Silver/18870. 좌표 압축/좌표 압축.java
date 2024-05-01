f9e294faffad339092f85433ba55a695904607ef
jeongyuje0ngyujeong <164482637+jeongyuje0ngyujeong@users.noreply.github.com>
2024-05-01 17:19:10 +0900

    [Silver II] Title: 좌표 압축, Time: 1872 ms, Memory: 265196 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -6,53 +6,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int size = Integer.parseInt(br.readLine());
-        IdxValue[] stdArr = new IdxValue[size];
+        int[] array = new int[size];
+        int[] copyArr = new int[size];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         for (int i = 0; i < size; i++) {
-            stdArr[i] = new IdxValue(Integer.parseInt(st.nextToken()), i, 0);
+            array[i] = Integer.parseInt(st.nextToken());
+            copyArr[i] = array[i];
         }
 
-        Arrays.sort(stdArr);
-        for (int i = 1; i < stdArr.length; i++) {
-            if (stdArr[i - 1].value == stdArr[i].value) {
-                stdArr[i].newValue = stdArr[i - 1].newValue;
-
-            } else {
-                stdArr[i].newValue = stdArr[i - 1].newValue + 1;
-
-            }
+        Arrays.sort(copyArr);
+        int cnt = 0;
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < size; i++) {
+            if (hashMap.containsKey(copyArr[i])) continue;
+            hashMap.put(copyArr[i], cnt++);
         }
 
-        Arrays.sort(stdArr, (Comparator.comparingInt(o -> o.idx)));
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < stdArr.length; i++) {
-            sb.append(stdArr[i].newValue).append(" ");
+        for (int i = 0; i < size; i++) {
+            sb.append(hashMap.get(array[i])).append(" ");
         }
 
         System.out.println(sb);
-    }
-}
-
-class IdxValue implements Comparable<IdxValue> {
-    int idx;
-    int value;
-    int newValue;
-
-    public IdxValue(int value, int idx, int newValue) {
-        this.idx = idx;
-        this.value = value;
-        this.newValue = newValue;
-    }
-
-    @Override
-    public int compareTo(IdxValue other) {
-        if (this.value < other.value) {
-            return -1;
-
-        } else if (this.value == other.value) {
-            return 0;
-
-        } else return 1;
     }
 }
