374300948b025e853c6a8b0e803c88021c2274e3
jeongyuje0ngyujeong <164482637+jeongyuje0ngyujeong@users.noreply.github.com>
2024-06-02 18:28:10 +0900

    [Silver II] Title: 예산, Time: 184 ms, Memory: 16032 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cityNum = Integer.parseInt(br.readLine());
+        int[] budget = new int[cityNum];
+        int sum = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < cityNum; i++) {
+            budget[i] = Integer.parseInt(st.nextToken());
+            sum += budget[i];
+        }
+
+        int totalBudget = Integer.parseInt(br.readLine());
+        Arrays.sort(budget);
+        if (sum <= totalBudget) {
+            System.out.println(budget[cityNum-1]);
+            return;
+        }
+
+        int start = 0;
+        int end = totalBudget;
+        int check = (start + end) / 2;
+        sum = 0;
+
+        while (start < check && check < end) {
+            sum = 0;
+            for (int i = 0; i < cityNum; i++) {
+                if (budget[i] <= check) {
+                    sum += budget[i];
+                } else {
+                    sum += check;
+                    if (sum > totalBudget) {
+                        break;
+                    }
+                }
+            }
+
+            if (sum > totalBudget) {
+                end = check;
+                check = (start + end) / 2;
+
+            } else if (sum < totalBudget) {
+                start = check;
+                check = (start + end) / 2;
+
+            } else {
+                break;
+            }
+        }
+
+        System.out.println(check);
+    }
+}
