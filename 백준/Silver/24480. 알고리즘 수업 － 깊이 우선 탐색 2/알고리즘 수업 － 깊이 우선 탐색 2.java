5147a1f14cd7069b72944756fdf4ffde2c58e73c
jeongyuje0ngyujeong <164482637+jeongyuje0ngyujeong@users.noreply.github.com>
2024-06-18 20:17:13 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 876 ms, Memory: 102084 KB -BaekjoonHub

--- 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -3,10 +3,9 @@ import java.util.*;
 
 public class Main {
     private static ArrayList<Integer>[] graph;
-    private static boolean[] isVisited;
+    private static int[] isVisited;
     private static int node = 0;
     private static int cnt = 1;
-    private static int[] answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,8 +16,7 @@ public class Main {
         int start = Integer.parseInt(st.nextToken());
 
         graph = new ArrayList[node + 1];
-        answer = new int[node + 1];
-        isVisited = new boolean[node + 1];
+        isVisited = new int[node + 1];
         for (int i = 0; i < node + 1; i++) {
             graph[i] = new ArrayList<>();
 
@@ -37,23 +35,22 @@ public class Main {
 
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= node; i++) {
-            sb.append(answer[i]).append("\n");
+            sb.append(isVisited[i]).append("\n");
         }
 
         System.out.println(sb);
     }
 
     private static void dfs(int start) {
-        if (isVisited[start]) {
+        if (isVisited[start] != 0) {
             return;
         }
 
-        isVisited[start] = true;
-        answer[start] = cnt++;
+        isVisited[start] = cnt++;
         Collections.sort(graph[start], Collections.reverseOrder());
 
         for (int i = 0; i < graph[start].size(); i++) {
-            if (!isVisited[graph[start].get(i)]) {
+            if (isVisited[graph[start].get(i)] == 0) {
                 dfs(graph[start].get(i));
             }
         }
