bfae861fec0803298e759de6f27614a9119a6c2c
jeongyuje0ngyujeong <164482637+jeongyuje0ngyujeong@users.noreply.github.com>
2024-07-10 15:54:20 +0900

    [Silver IV] Title: 스택, Time: 312 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,36 @@
+num = int(input())
+stack = []
+ans_list = []
+for i in range(num):
+    check = list(map(str, input().split()))
+
+    if len(check) != 1:
+        stack.append(int(check[1]))
+
+    else:
+        if check[0] == "pop":
+            if stack:
+                ans_list.append(stack.pop())
+
+            else:
+                ans_list.append(-1)
+
+        elif check[0] == "size":
+            ans_list.append(len(stack))
+
+        elif check[0] == "empty":
+            if stack:
+                ans_list.append(0)
+
+            else:
+                ans_list.append(1)
+
+        elif check[0] == "top":
+            if stack:
+                (ans_list.append(stack[len(stack) - 1]))
+
+            else:
+                ans_list.append(-1)
+
+for ans in ans_list:
+    print(ans)
