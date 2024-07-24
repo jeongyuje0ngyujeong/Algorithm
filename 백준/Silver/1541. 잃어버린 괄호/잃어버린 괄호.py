2ab0bc8e60bf155c8b20a4872b48eaafc5140c3b
jeongyuje0ngyujeong <164482637+jeongyuje0ngyujeong@users.noreply.github.com>
2024-07-24 18:32:16 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,29 +1,11 @@
 import sys
 
 math_expression = sys.stdin.readline().strip()
-if '+' in math_expression:
-    split_first = list(map(str, math_expression.split('-')))
-    for i in range(0, len(split_first)):
-        if '+' in split_first[i]:
-            check_num = 0
-            split_second = split_first[i].split('+')
-            for check in split_second:
-                check_num += int(check)
-            split_first[i] = check_num
+after_minus = math_expression.split('-')
+answer = 0
 
-        else:
-            split_first[i] = int(split_first[i])
+answer += sum(map(int, (after_minus[0].split('+'))))
 
-    answer = split_first[0]
-    for i in range(1, len(split_first)):
-        answer -= split_first[i]
-
-    sys.stdout.write(str(answer))
-
-else:
-    numbers = list(map(int, math_expression.split('-')))
-    answer = numbers[0]
-    for i in range(1, len(numbers)):
-        answer -= numbers[i]
-
-    sys.stdout.write(str(answer))
+for num in after_minus[1:]:
+    answer -= sum(map(int, (num.split('+'))))
+sys.stdout.write(str(answer))
