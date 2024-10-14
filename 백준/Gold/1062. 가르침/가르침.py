2339c6dfb4e7f72a7e24a0f82ed7c477d78d47aa
jeongyuje0ngyujeong <164482637+jeongyuje0ngyujeong@users.noreply.github.com>
2024-10-14 16:35:01 +0900

    [Gold IV] Title: 가르침, Time: 2932 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1062. 가르침/가르침.py
@@ -0,0 +1,55 @@
+import sys
+sys.setrecursionlimit(1000000)
+
+words_num, alphabet_num = map(int, sys.stdin.readline().strip().split())
+
+if alphabet_num < 5:
+    print(0)
+    exit()
+
+if alphabet_num == 26:
+    print(words_num)
+    exit()
+
+# words = [list(input().rstrip()) for _ in range(words_num)]
+words = []
+for i in range(words_num):
+    word = sys.stdin.readline().strip()
+    words.append(set(word) - set('antic'))
+
+legible_alpha = [0] * 26
+
+for w in ('a', 'n', 't', 'i', 'c'):
+    legible_alpha[ord(w) - ord('a')] = True
+
+answer = 0
+
+
+def dfs(idx: int, cnt: int) -> None:
+    global answer
+
+    if cnt == alphabet_num - 5:
+        read_cnt = 0
+        for word in words:
+            flag = True
+
+            for w in word:
+                if not legible_alpha[ord(w) - ord('a')]:
+                    flag = False
+                    break
+
+            if flag:
+                read_cnt += 1
+
+        answer = max(answer, read_cnt)
+        return
+
+    for i in range(idx, 26):
+        if not legible_alpha[i]:
+            legible_alpha[i] = True
+            dfs(i, cnt + 1)
+            legible_alpha[i] = False
+
+
+dfs(0, 0)
+print(answer)
