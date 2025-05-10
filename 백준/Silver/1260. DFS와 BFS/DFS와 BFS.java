91ce6fa81f266c2c9513008518247ce0cf8968dc
jeongyuje0ngyujeong <jeongyuje0ngyujeong.j@gmail.com>
2025-05-10 15:06:13 +0900

    [Silver II] Title: DFS와 BFS, Time: 208 ms, Memory: 20152 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,60 +2,70 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static int[][] map;
-    private static boolean[] isVisited;
-    private static StringBuilder sb = new StringBuilder();
+    static boolean[] isVisited;
+    static StringBuilder sb = new StringBuilder();
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int nodes = Integer.parseInt(st.nextToken());
-        int edges = Integer.parseInt(st.nextToken());
-        int searchStart = Integer.parseInt(st.nextToken());
+        String[] graphInfo = br.readLine().split(" ");
+        int vertex = Integer.parseInt(graphInfo[0]);
+        int edge = Integer.parseInt(graphInfo[1]);
+        int startVertex = Integer.parseInt(graphInfo[2]);
+        isVisited = new boolean[vertex + 1];
 
-        map = new int[nodes + 1][nodes + 1];
-        isVisited = new boolean[nodes + 1];
-        isVisited[0] = true;
-
-        for (int i = 0; i < edges; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            map[start][end] = map[end][start] = 1;
+        for (int i = 0; i <= vertex; i++) {
+            graph.add(new ArrayList<>());
         }
 
-        dfs(searchStart);
-        sb.append("\n");
-        bfs(searchStart);
-        
+        StringTokenizer st;
+        for (int i = 0; i < edge; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        for (int i = 1; i <= vertex; i++) {
+            Collections.sort(graph.get(i));
+        }
+        dfs(startVertex);
+        sb.delete(sb.length()-1, sb.length());
+        System.out.println(sb);
+
+        Arrays.fill(isVisited, false);
+        sb.setLength(0);
+        bfs(startVertex);
+        sb.delete(sb.length()-1, sb.length());
         System.out.println(sb);
     }
 
-    private static void dfs(int searchStartNum) {
-        if (!isVisited[searchStartNum]) {
-            sb.append(searchStartNum).append(" ");
-            isVisited[searchStartNum] = true;
-            for (int i = 1; i < map.length; i++) {
-                if (map[searchStartNum][i] == 1 && !isVisited[i]) dfs(i);
-            }
+    public static void dfs(int cur) {
+        if (isVisited[cur]) return;
+
+        isVisited[cur] = true;
+        sb.append(cur).append(" ");
+        for (int i = 0; i < graph.get(cur).size(); i++) {
+            dfs(graph.get(cur).get(i));
         }
+        return;
     }
 
-    private static void bfs(int searStartNum) {
-        Queue<Integer> bucket = new LinkedList<>();
-        isVisited = new boolean[map.length];
-        isVisited[0] = true;
+    public static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        isVisited[start] = true;
 
-        bucket.add(searStartNum);
-        while (!bucket.isEmpty()) {
-            int check = bucket.poll();
-            sb.append(check).append(" ");
-            isVisited[check] = true;
-
-            for (int i = 1; i < map[check].length; i++) {
-                if (map[check][i] == 1 && !isVisited[i]) {
-                    isVisited[i] = true;
-                    bucket.add(i);
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            sb.append(cur).append(" ");
+            for (int i = 0; i < graph.get(cur).size(); i++) {
+                int check = graph.get(cur).get(i);
+                if (!isVisited[check]) {
+                    isVisited[check] = true;
+                    queue.add(check);
                 }
             }
         }
