b075433d166afa025d0f31fece58ec839768fe15
jeongyuje0ngyujeong <jeongyuje0ngyujeong.j@gmail.com>
2025-08-08 22:21:22 +0900

    [Silver II] Title: 유기농 배추, Time: 148 ms, Memory: 16132 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,75 +1,53 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static boolean[][] isVisited;
-    static int[][] map;
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int testCase = Integer.parseInt(br.readLine());
-        int[] cabbageArea = new int[testCase];
-        for (int i = 0; i < testCase; i++) {
 
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int mapX = Integer.parseInt(st.nextToken());
-            int mapY = Integer.parseInt(st.nextToken());
-            int point = Integer.parseInt(st.nextToken());
+        while (testCase != 0) {
+            testCase--;
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int col = Integer.parseInt(st.nextToken());
+            int row = Integer.parseInt(st.nextToken());
+            int cabNum = Integer.parseInt(st.nextToken());
 
-            map = new int[mapY][mapX];
-            isVisited = new boolean[mapY][mapX];
-
-            for (int idx = 0; idx < point; idx++) {
-                st = new StringTokenizer(br.readLine());
-                int pointX = Integer.parseInt(st.nextToken());
-                int pointY = Integer.parseInt(st.nextToken());
-
-                map[pointY][pointX] = 1;
+            int[][] map = new int[row][col];
+            for (int i = 0; i < cabNum; i++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                map[y][x] = 1;
             }
 
-            for (int y = 0; y < mapY; y++) {
-                for (int x = 0; x < mapX; x++) {
-                    if (map[y][x] == 1 && !isVisited[y][x]) {
-                        isVisited[y][x] = true;
-                        Bfs(x, y);
-                        cabbageArea[i]++;
+            Queue<int[]> queue;
+            int wormNum = 0;
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (map[i][j] == 1) {
+                        queue = new LinkedList<>();
+                        queue.add(new int[] {i, j});
+                        map[i][j]--;
+                        int[] dx = {0, 0, -1, 1};
+                        int[] dy = {-1, 1, 0, 0};
+                        while (!queue.isEmpty()) {
+                            int[] cur = queue.poll();
+                            for (int k = 0; k < 4; k++) {
+                                int moveX = cur[1] + dx[k];
+                                int moveY = cur[0] + dy[k];
+                                if (col <= moveX || moveX < 0 || row <= moveY || moveY < 0 || map[moveY][moveX] != 1) {
+                                    continue;
+                                }
+                                queue.add(new int[] {moveY, moveX});
+                                map[moveY][moveX]--;
+                            }
+                        }
+                        wormNum++;
                     }
                 }
             }
-
-        }
-
-        for (int answer : cabbageArea) {
-            System.out.println(answer);
-        }
-    }
-
-    private static void Bfs(int x, int y) {
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {-1, 1, 0, 0};
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] {y, x});
-
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int moveX = dx[i] + current[1];
-                int moveY = dy[i] + current[0];
-                int mapColumn = map[0].length;
-                int mapRow = map.length;
-
-                if (moveX >= mapColumn || moveX < 0 || moveY >= mapRow || moveY < 0) {
-                    continue;
-                }
-
-                if (map[moveY][moveX] == 1 && ! isVisited[moveY][moveX]){
-                    isVisited[moveY][moveX] = true;
-                    queue.add(new int[] {moveY, moveX});
-                }
-            }
+            System.out.println(wormNum);
         }
     }
 }
